56bbbe16d0de1be92b78e8d0f80002a3553f4eb1
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2024-01-06 10:57:13 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/creatinganotations/CreatingAnotationsApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.creatinganotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingAnotationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingAnotationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/seteideias/creatinganotations/CreatingAnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.creatinganotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingAnotationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
